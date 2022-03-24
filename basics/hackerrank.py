ef414c89bf3e38411f7cfb081c24a3620c3b6a8c
gurpreetreact <gurpreet.reactdev@gmail.com>
2022-03-25 00:13:22 +0530

    hackerrank and a matrix problem

---
+++ basics/hackerrank.py
@@ -0,0 +1,15 @@
+# More than 2 lines will result in 0 score. Do not leave a blank line also
+for i in range(1, int(input('Enter'))):
+    print(int((10 ** (i) - 1)/9)*i)
+
+# Result
+# Input: 9
+# Output
+# 1
+# 22
+# 333
+# 4444
+# 55555
+# 666666
+# 7777777
+# 88888888
